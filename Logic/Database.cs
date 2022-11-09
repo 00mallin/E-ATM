895857416d530ba76409714e357ae4c63a04a111
Mohammed7m <113667097+Mohammed7m@users.noreply.github.com>
2022-11-09 15:20:49 +0100

    GetCard medtod i Databas class

--- Logic/Database.cs
+++ Logic/Database.cs
@@ -1,8 +1,9 @@
 using MySqlConnector;
+using Dapper;
 
 namespace Logic;
 
-class Database
+public class Database
 {
     public MySqlConnection Connection { get; private set; }
     private string connectionString = "Server=familjenlindh.se;Database=e_atm;Uid=eatm;Pwd=atm123";
@@ -10,5 +11,19 @@ class Database
     public Database()
     {
         Connection = new(connectionString);
+    }
+
+    public bool GetCard(string CardNumber)
+    {
+        var Cardfound = Connection.QuerySingleOrDefault($"SELECT COUNT(id) FROM card WHERE card_number = '{CardNumber}'");
+
+        if (Cardfound >= 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
