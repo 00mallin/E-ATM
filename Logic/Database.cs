c043ef11d9e88af7e2a7b0958c3d98df2dea5661
00mallin <00mallin@skola.boras.se>
2022-11-09 13:41:12 +0100

    Connection to database added

--- Logic/Database.cs
+++ Logic/Database.cs
@@ -1,12 +1,14 @@
-namespace Logic;
 using MySqlConnector;
+
+namespace Logic;
 
 class Database
 {
     public MySqlConnection Connection { get; private set; }
+    private string connectionString = "Server=familjenlindh.se;Database=e_atm;Uid=eatm;Pwd=atm123";
 
     public Database()
     {
-
+        Connection = new(connectionString);
     }
 }
