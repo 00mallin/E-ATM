07b11d024b3b3a3e6ab05df959fc8a8fdb6233dc
Mohammed7m <113667097+Mohammed7m@users.noreply.github.com>
2022-11-09 11:09:32 +0100

    skapat Card class

---
+++ Logic/Card.cs
@@ -0,0 +1,8 @@
+public class Card
+{
+    public int Pin { get; set; }
+    public string CardNumber { get; set; }
+    public string CardHolder { get; set; }
+    public DateTime ExpiryDate { get; set; }
+
+}
