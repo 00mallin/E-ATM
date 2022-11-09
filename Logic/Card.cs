048e60c0a8392fad6267374fb2fdebba91d3a3c0
00mallin <00mallin@skola.boras.se>
2022-11-09 14:03:07 +0100

    Added IsValid and ID to Card class

--- Logic/Card.cs
+++ Logic/Card.cs
@@ -1,8 +1,11 @@
+namespace Logic;
+
 public class Card
 {
-    public int Pin { get; set; }
+    public int ID { get; set; }
     public string CardNumber { get; set; }
     public string CardHolder { get; set; }
     public DateTime ExpiryDate { get; set; }
+    public bool IsValid { get; set; }
 
 }
