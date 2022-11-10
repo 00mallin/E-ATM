00e93eb1c5185ebfad2ed5667e182b55214522ea
00mallin <00mallin@skola.boras.se>
2022-11-10 14:32:52 +0100

    Added AccountID to Card

--- Logic/Card.cs
+++ Logic/Card.cs
@@ -3,9 +3,12 @@ namespace Logic;
 public class Card
 {
     public int ID { get; set; }
+    public int AccountID { get; set; }
     public string CardNumber { get; set; }
     public string CardHolder { get; set; }
     public DateTime ExpiryDate { get; set; }
     public bool IsValid { get; set; }
+
+    public Card(){}
 
 }
