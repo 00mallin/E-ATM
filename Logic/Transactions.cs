2ad7ab413a8e2a7c481e16aa582678b36ac2d7fb
00mallin <00mallin@skola.boras.se>
2022-11-17 13:18:20 +0100

    Changed ToString method

--- Logic/Transactions.cs
+++ Logic/Transactions.cs
@@ -6,6 +6,6 @@ public class Transactions
 
     public override string ToString()
     {
-        return ID + ".  Amount: " + Amount + " kr  " + Date;
+        return "Amount: " + Amount + " kr  " + Date.ToShortDateString();
     }
 }
