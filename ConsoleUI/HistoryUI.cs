5742f2598980965d61208478954f12e1eca2df23
Mohammed7m <113667097+Mohammed7m@users.noreply.github.com>
2022-11-17 12:18:30 +0100

    HistoryUI class klar

--- ConsoleUI/HistoryUI.cs
+++ ConsoleUI/HistoryUI.cs
@@ -4,6 +4,18 @@ public class HistoryUI
 {
     public static void Show(Card card)
     {
-        
+        Console.Clear();
+        Database db = new();
+
+        Account selectedAccount = db.GetAccount(card.AccountID);
+        Transactions accountTransaction = new();
+
+        foreach (var item in selectedAccount.GetTransactions())
+        {
+            Console.Write(item);
+            accountTransaction.ToString();
+            Console.ReadKey();
+        }
+
     }
 }
