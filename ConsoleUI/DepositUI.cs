3ccba330b06f06c3c619b0e7a791dd32f4ccc3ee
00mallin <00mallin@skola.boras.se>
2022-11-17 10:22:38 +0100

    Fixed bug with account selection

--- ConsoleUI/DepositUI.cs
+++ ConsoleUI/DepositUI.cs
@@ -4,35 +4,45 @@ public class DepositUI
 {
     public static void Show(Card card)
     {
-        Console.Clear();
         Database db = new();
         List<Account> userAccounts = db.GetUserAccounts(card.UserID);
 
-        for (int i = 0; i < userAccounts.Count; i++)
+        while (true)
         {
-            Console.WriteLine($"{i + 1}. {userAccounts[i]}");
+            Console.Clear();
+
+            for (int i = 0; i < userAccounts.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {userAccounts[i]}");
+            }
+
+            Console.Write("Select account: ");
+            int num = Int32.Parse(Console.ReadLine());
+
+            if (num <= 0 || num > userAccounts.Count)
+            {
+                Console.WriteLine("Invalid selection. Press any key to try again.");
+                Console.ReadKey();
+                continue;
+            }
+
+            Account selectedAccount = userAccounts[num - 1];
+
+
+            Console.Clear();
+            Console.WriteLine("You have: " + selectedAccount.Balance + " kr in your account.");
+
+            Console.Write("Input the amount you would like to insert: ");
+            float input = float.Parse(Console.ReadLine());
+            Console.Clear();
+
+            selectedAccount.Deposit(input);
+
+            Console.WriteLine($"Success! Your account balance: {selectedAccount.Balance} kr");
+            Console.Write("Press any key to return to menu...");
+            Console.ReadKey();
+            break;
         }
-
-        Console.Write("Select account: ");
-        int num = Int32.Parse(Console.ReadLine());
-
-        if (num >= 0 && num > userAccounts.Count)
-        {
-            Console.WriteLine("Invalid selection.");
-            return;
-        }
-
-        Account selectedAccount = userAccounts[num - 1];
-
-        Console.WriteLine("You have: " + selectedAccount.Balance + " kr in your account.");
-
-        Console.Write("Input the amount you would like to insert: ");
-        float input = float.Parse(Console.ReadLine());
-
-        selectedAccount.Deposit(input);
-
-        selectedAccount.ToString();
-
     }
 
 }
