a09e402dbe8eeb566b5190dfa2c84066efb99b16
Kasper Alsén <alsenkasper@gmail.com>
2022-11-22 12:17:38 +0100

    Changed float to decimal for a larger range

--- Logic/Transactions.cs
+++ Logic/Transactions.cs
@@ -2,7 +2,7 @@ public class Transactions
 {
     public int ID { get; set; }
     public DateTime Date { get; set; }
-    public float Amount { get; set; }
+    public decimal Amount { get; set; }
 
     public override string ToString()
     {
